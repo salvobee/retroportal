cc624ddde614ab9332ba09fd15ed3abf971b27d1
Salvo Bonanno <salvob.labs@gmail.com>
2025-09-20 07:06:17 +0200

    Moves theme values to config entry

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,30 +26,7 @@ class AppServiceProvider extends ServiceProvider
             if (!$mode) {
                 $mode = request()->cookie('theme', 'light');
             }
-
-            // Ultra-compatible palettes (HTML 3.2 / <body> attributes)
-            $palettes = [
-                'light' => [
-                    'bg'    => '#ffffff',
-                    'text'  => '#000000',
-                    'link'  => '#0000ff',
-                    'vlink' => '#660099',
-                    'alink' => '#ff0000',
-                    'border'=> '#cccccc',
-                    'muted' => '#333333',
-                ],
-                'dark' => [
-                    'bg'    => '#000000',
-                    'text'  => '#e6e6e6',
-                    'link'  => '#9ecbff',
-                    'vlink' => '#c5a3ff',
-                    'alink' => '#ff7a7a',
-                    'border'=> '#444444',
-                    'muted' => '#aaaaaa',
-                ],
-            ];
-
-            $palette = $palettes[$mode] ?? $palettes['light'];
+            $palette = config("palettes.$mode") ?? config("palettes.light");
 
             $view->with('theme_mode', $mode)
                 ->with('theme_palette', $palette);
