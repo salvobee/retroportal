9ab530f9f13db6cdad5c29b6cad1d1067bd56666
Salvo Bonanno <salvob.labs@gmail.com>
2025-09-21 16:59:28 +0200

    Fixes home link for news section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,7 +7,7 @@
     <p>{{ __('This portal provides modern capabilities with a retro-compatible interface.') }}</p>
     <ul>
         <li><a href="{{ route('features.search') }}">{{ __('web-search.title') }}</a></li>
-        <li><a href="{{ route('features.news') }}">{{ __('news.title') }}</a></li>
+        <li><a href="{{ route('features.news.index') }}">{{ __('news.title') }}</a></li>
         <li><a href="{{ route('features.weather.form') }}">{{ __('weather.title') }}</a></li>
         <li><a href="{{ route('features.wikipedia') }}">{{ __('encyclopedia.title') }}</a></li>
         <li><a href="{{ route('chatbot.index') }}">{{ __('ai.title') }}</a></li>
