cb0cc370e38af254cfbb51a72ba541f5e1664554
Salvo Bonanno <salvob.labs@gmail.com>
2025-10-18 14:40:46 +0200

    Removes unused routes

--- routes/auth.php
+++ routes/auth.php
@@ -43,14 +43,14 @@ Route::middleware('auth')->group(function () {
 //        ->middleware(['signed', 'throttle:6,1'])
 //        ->name('verification.verify');
 
-    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-        ->middleware('throttle:6,1')
-        ->name('verification.send');
-
-    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
-        ->name('password.confirm');
-
-    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+//    Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+//        ->middleware('throttle:6,1')
+//        ->name('verification.send');
+//
+//    Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+//        ->name('password.confirm');
+//
+//    Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
