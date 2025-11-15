a2201b5daf64c3904cfdac46f54f5de03a3347f0
Salvo Bonanno <salvob.labs@gmail.com>
2025-11-15 08:44:30 +0100

    Adds inspector

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use \Inspector\Laravel\Middleware\WebRequestMonitoring;
 
     return Application::configure(basePath: dirname(__DIR__))
         ->withRouting(
@@ -11,7 +12,9 @@ use Illuminate\Foundation\Configuration\Middleware;
             health: '/up',
         )
         ->withMiddleware(function (Middleware $middleware): void {
-            $middleware->appendToGroup('web', \App\Http\Middleware\SetLocale::class);
+            $middleware->appendToGroup('web', \App\Http\Middleware\SetLocale::class)
+                ->appendToGroup('web', WebRequestMonitoring::class)
+                ->appendToGroup('api', WebRequestMonitoring::class);
         })
         ->withExceptions(function (Exceptions $exceptions): void {
             //
